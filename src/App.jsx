b2859ceee1325cf0864057a39ed4af8d59bd7cee
akashmondal2000 <mondalakash494@gmail.com>
2024-06-13 01:28:02 +0530

    create context api, card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from "react";
+
 import "./App.css";
 
 const App = () => {
 
-  const [count,setCount]= useState(0);
-
-  const add =()=>{
-    setCount(count+1);
-  }
-  const minus =()=>{
-    setCount(count-1);
-  }
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={add}>add</button>
-      <button className=" ml-8" onClick={minus}>minus</button>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* themeButton */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          {/* card */}
+        </div>
+      </div>
     </div>
   );
 };
